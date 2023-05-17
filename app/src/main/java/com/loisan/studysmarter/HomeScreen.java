0ab72976761891946a348411bc3675cf2b06f0bd
loiy al-hasan <lloiy9310@gmail.com>
2023-05-17 19:12:23 +0300

    UPD activity_home.xml

--- app/src/main/java/com/loisan/studysmarter/HomeScreen.java
+++ app/src/main/java/com/loisan/studysmarter/HomeScreen.java
@@ -9,6 +9,6 @@ public class HomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 }
