0e3cb4fff8c65f7e6a1c517d7bd033a8a9b72141
loiy al-hasan <lloiy9310@gmail.com>
2023-05-19 15:50:41 +0300

    Support Responsive text in land and portal

--- app/src/main/java/com/loisan/studysmarter/HomeScreen.java
+++ app/src/main/java/com/loisan/studysmarter/HomeScreen.java
@@ -3,12 +3,27 @@ package com.loisan.studysmarter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeScreen extends AppCompatActivity {
+
+
+    TextView home_setting_tv, home_points_tv, home_store_tv,
+             home_timer_tv, home_start_pause_resume_tv, home_reset_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        home_setting_tv = findViewById(R.id.home_setting_tv);
+        home_points_tv = findViewById(R.id.home_points_tv);
+        home_store_tv = findViewById(R.id.home_store_tv);
+        home_timer_tv = findViewById(R.id.home_timer_tv);
+        home_start_pause_resume_tv = findViewById(R.id.home_start_pause_resume_tv);
+        home_reset_tv = findViewById(R.id.home_reset_tv);
+
+
+
     }
 }
